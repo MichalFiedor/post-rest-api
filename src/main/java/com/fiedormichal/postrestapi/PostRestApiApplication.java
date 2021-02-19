18f226cb5b5a06cdbdf9a2d264c80ea13de8a4bb
MichalFiedor <michal.fiedor92@gmail.com>
2021-02-19 15:28:34 +0100

    Project configuration.

---
+++ src/main/java/com/fiedormichal/postrestapi/PostRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.fiedormichal.postrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiedormichal/postrestapi/PostRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiedormichal.postrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
