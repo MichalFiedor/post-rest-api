c0889086dd9bab52d3067e3a434f6f2c24302a08
MichalFiedor <michal.fiedor92@gmail.com>
2021-02-19 19:22:25 +0100

    PostService methods for posts have been added.

--- src/main/java/com/fiedormichal/postrestapi/controller/PostController.java
+++ src/main/java/com/fiedormichal/postrestapi/controller/PostController.java
@@ -1,10 +1,28 @@
 package com.fiedormichal.postrestapi.controller;
 
+import com.fiedormichal.postrestapi.model.Post;
+import com.fiedormichal.postrestapi.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class PostController {
+    private final PostService postService;
 
+    @GetMapping("/REST")
+    @ResponseStatus(value = HttpStatus.OK)
+    public void updateAndShowAllPosts(){
+        postService.updatedPosts();
+    }
+
+    @GetMapping("/posts")
+    public List<Post> getAllPosts(){
+        return postService.findAll();
+    }
 }
